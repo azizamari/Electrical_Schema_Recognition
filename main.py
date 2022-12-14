a14fb4ef4f964fb7c5a336dc48cd1d1911b79e86
azizamari <azizamari2003@gmail.com>
2022-12-14 14:55:49 +0100

    create fast api backend with 3 endpoints

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, File
+from utils import get_yolov5, get_image_from_bytes
+from starlette.responses import Response
+import io
+from PIL import Image
+import json
+from fastapi.middleware.cors import CORSMiddleware
+model = get_yolov5()
+app = FastAPI(
+    title="Circuit Builder Project by Aziz Amari",
+    description="""this api can fully understand a handwritten
+     electrical circuit (components and connections recognition)""",
+    version="0.0.1",
+)
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/notify/v1/health')
+def get_health():
+    return dict(msg='OK')
+
+@app.post("/object-to-json")
+async def detect_food_return_json_result(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")
+    detect_res = json.loads(detect_res)
+    return {"result": detect_res}
+
+@app.post("/object-to-img")
+async def detect_food_return_base64_img(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    results.render()  # updates results.imgs with boxes and labels
+    for img in results.imgs:
+        bytes_io = io.BytesIO()
+        img_base64 = Image.fromarray(img)
+        img_base64.save(bytes_io, format="jpeg")
+    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
