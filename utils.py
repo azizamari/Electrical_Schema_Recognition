16a33525ed88b308d7a56190112b3025311be9ba
azizamari <azizamari2003@gmail.com>
2022-12-14 14:51:48 +0100

    define get_image_from_bytes in utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import io
+
+
+def get_image_from_bytes(binary_image, max_size=1024):
+    input_image =Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize((
+        int(input_image.width * resize_factor),
+        int(input_image.height * resize_factor)
+    ))
+    return resized_image
