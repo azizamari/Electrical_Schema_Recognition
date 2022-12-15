5092bcd45c9b634808ed3c59040d905dd1f4d310
azizamari <azizamari2003@gmail.com>
2022-12-15 10:48:15 +0100

    add deleting componen feature

---
+++ node_recognition/delete_components.py
@@ -0,0 +1,15 @@
+import json
+import cv2
+import numpy as np
+
+with open('test.json','r') as f:
+    dict=json.load(f)['result']
+
+img=cv2.imread('test.jpg',0)
+for box in dict:
+    coordinates=[[int(box['xmin']),int(box['ymin'])],[int(box['xmax']),int(box['ymax'])]]
+    for i in range(2):
+        for j in range(2):
+            coordinates[i][j]=min(coordinates[i][j],320)
+    cv2.rectangle(img,*coordinates, 255, -1)
+cv2.imwrite('no_boxes.jpg',img)
